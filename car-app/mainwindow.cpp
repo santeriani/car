b31804577eb39ac421a284721ac99b41f9739593
santeriani <santeri.anias@gmail.com>
2026-02-17 19:23:46 +0200

    qtssa sovelluksen ja hae functionin tekoa

--- car-app/mainwindow.cpp
+++ car-app/mainwindow.cpp
@@ -6,9 +6,51 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->btnGet, &QPushButton::clicked, this, &MainWindow::btnGetSlot);
+    manager = new QNetworkAccessManager(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::btnGetSlot()
+{
+    qDebug()<<"pushed";
+
+
+    QString site_url = "http://127.0.0.1:3000/car";
+    QNetworkRequest request(site_url);
+
+    reply = manager->get(request);
+
+    connect(reply, &QNetworkReply::finished,
+            this, &MainWindow::getCarSlot);
+}
+
+void MainWindow::getCarSlot()
+{
+    response_data = reply->readAll();
+    qDebug() << "DATA:" << response_data;
+
+    QJsonDocument json_doc = QJsonDocument::fromJson(response_data);
+    QJsonArray json_array = json_doc.array();
+
+    ui->listWidget->clear();
+
+    for (int i = 0; i < json_array.size(); i++)
+    {
+        QJsonObject json_obj = json_array[i].toObject();
+
+        int id = json_obj["id_car"].toInt();
+        QString brand = json_obj["branch"].toString();
+        QString model = json_obj["model"].toString();
+
+        QString car = QString::number(id) + " | " + brand + " " + model;
+
+        ui->listWidget->addItem(car);
+    }
+
+    reply->deleteLater();
 }
--- car-app/mainwindow.h
+++ car-app/mainwindow.h
@@ -1,6 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include "environment.h"
+
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -17,7 +25,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void btnGetSlot();
+    void getCarSlot();
+
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
+    QNetworkReply *reply;
+    QByteArray response_data;
+
 };
 #endif // MAINWINDOW_H
